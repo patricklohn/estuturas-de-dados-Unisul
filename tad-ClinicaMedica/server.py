5a3f4443c77975742dfb1130b2b80b9624dc0d94
patricklohn <patricklohn3@gmail.com>
2025-03-19 07:49:27 -0300

    Inicio da etapa 2

---
+++ tad-ClinicaMedica/server.py
@@ -0,0 +1,13 @@
+def entradaPaciente():
+    print("Nome do paciente:")
+    namePaciente = input()
+    print("Numero da carteirinha:")
+    numCarteirinha = input()
+    print("Descreva is sintomas:")
+    sintomas = input()
+    print("Descreva a urgencia entre: 1 - 10")
+    urgencia = input()
+
+def chamadaCliente():
+    print("Teste")
+
