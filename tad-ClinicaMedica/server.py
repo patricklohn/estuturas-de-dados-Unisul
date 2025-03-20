ea7dfed222f37b3896d7c5fa315858bbbe559d7b
patricklohn <patricklohn3@gmail.com>
2025-03-20 07:57:17 -0300

    Criado funções para implmentação e controle de navegação

--- tad-ClinicaMedica/server.py
+++ tad-ClinicaMedica/server.py
@@ -1,12 +1,23 @@
 
-paciente = []
+paciente = {}
+pacientesUrgentes = {}
 
-class Paciente: 
-    def __init__(self, nome, numCarteirinha, sintomas, urgencia):
-        self.nome = nome
-        self.numCarteirinha = numCarteirinha
-        self.sintomas = sintomas
-        self.urgencia = urgencia
+def menu():
+    print("Coloque a sua opção de navegação:")
+    print("1 - Entrada de paciente")
+    print("2 - Listar pacientes")
+    print("6 - Sair ")
+
+    opcao = input()
+
+    if opcao == "1":
+        entradaPaciente()
+    elif opcao == "2":
+        listarPacientes()
+    elif opcao == "6":
+        print("Obrigado por usar o sistema!")
+    else:
+        print("Obrigado por usar o sistema!")
 
 def entradaPaciente():
     print("Nome do paciente:")
@@ -16,10 +27,37 @@ def entradaPaciente():
     print("Descreva is sintomas:")
     sintomas = input()
     print("Descreva a urgencia entre: 1 - 10")
-    urgencia = input()
+    urgencia = input() 
+
+    if int(urgencia) >= 6:
+        pacientesUrgentes[namePaciente] ={
+            "carteira": numCarteirinha,
+            "sintomas": sintomas,
+            "urgencia": urgencia
+        }
+    else: 
+        paciente[namePaciente] = {
+            "carteira": numCarteirinha,
+            "sintomas": sintomas,
+            "urgencia": urgencia
+        }
+    print("Paciente protocolado com sucesso")
+    menu()
+
+def listarPacientes():
+    print("Pacientes urgentes:")
+    for key, value in pacientesUrgentes.items():
+        print(key,value)
+    print("Pacientes não urgentes:")
+    for key, value in paciente.items():
+        print(key,value)
+    menu()
+
+print("Bem vindo ao ao sistema clinico de triagem:")
+menu()
 
 
 
-def chamadaCliente():
-    print("Teste")
+
+
 
