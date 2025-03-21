6ea9b76f6f407ffb3e068ccafcde9f1a3c527f6e
patricklohn <patricklohn3@gmail.com>
2025-03-20 22:51:50 -0300

    Finalização do codigo

--- tad-ClinicaMedica/server.py
+++ tad-ClinicaMedica/server.py
@@ -1,14 +1,15 @@
 
-pacientesNaoUrgentes = []
-pacientesUrgentes = []
+pacientesNaoUrgentes = [{'nome': 'Erick', 'carteira': '147', 'sintomas': 'Dor de cabeça', 'urgencia': 5},{'nome': 'Rafael', 'carteira': '12347', 'sintomas': 'Muita febre', 'urgencia': 4}]
+pacientesUrgentes = [{'nome': 'Patrick Lohn', 'carteira': '1234', 'sintomas': 'Muita tosse', 'urgencia': 8},{'nome': 'Andrey', 'carteira': '12345', 'sintomas': 'Gripe', 'urgencia': 8}]
 pacienteAtendidos = []
+global intervalPaciente
 intervalPaciente = 0
-pacientesAtendidos = []
 
 def menu():
     print("\nColoque a sua opção de navegação: \n")
     print("1 - Entrada de paciente")
     print("2 - Listar pacientes")
+    print("3 - Triagem")
     print("6 - Sair \n")
 
     opcao = input()
@@ -17,6 +18,8 @@ def menu():
         entradaPaciente()
     elif opcao == "2":
         listarPacientes()
+    elif opcao == "3":
+        triagem()
     elif opcao == "6":
         print("Obrigado por usar o sistema!")
     else:
@@ -42,7 +45,7 @@ def entradaPaciente():
     namePaciente = input()
     print("Numero da carteirinha:")
     numCarteirinha = input()
-    print("Descreva is sintomas:")
+    print("Descreva os sintomas:")
     sintomas = input()
     print("Descreva a urgencia entre: 1 - 10")
     urgencia = input() 
@@ -62,38 +65,55 @@ def listarPacientes():
         print(dadosPaciente) 
     print("\n")
 
-menu()
+    print("Pacientes Atendidos:\n")
+    for dadosPaciente in pacienteAtendidos:
+        print(dadosPaciente) 
+    print("\n")
+
+    menu()
 
 def triagem():
-    print("Chamando paciente...\n")
-    if not pacientesUrgentes[0]:
-        intervalPaciente = 1
-    elif not pacientesNaoUrgentes[0]:
-        intervalPaciente = 0
+    if not pacientesUrgentes and not pacientesNaoUrgentes:
         print("Não existe pacientes na triagem:") 
         return menu()
+    
+    if not pacientesUrgentes:
+        intervalPaciente = 2
+        print("\nChamando paciente...\n")
+
+    elif not pacientesNaoUrgentes:
+        intervalPaciente = 0
+        print("\nChamando paciente...\n")
+
     else:
-        print("Chamando paciente...\n")
-    if intervalPaciente == 0: 
+        intervalPaciente = 0
+
+    if intervalPaciente == 0 or intervalPaciente == 1: 
         print("Paciente urgente:", pacientesUrgentes[0].get("nome"))
         print("Numero Carteirinha:", pacientesUrgentes[0].get("carteira"))
         print("Sintomas reportados:", pacientesUrgentes[0].get("sintomas"))
 
         pacienteAtendidos.append(pacientesUrgentes[0])
         pacientesUrgentes.pop(0)
+        intervalPaciente += 1
         print("Paciente Atendido com sucesso!")
-    elif intervalPaciente == 1:
+        return menu()
+
+    elif intervalPaciente == 2:
         print("Paciente:", pacientesNaoUrgentes[0].get("nome"))
         print("Numero:", pacientesNaoUrgentes[0].get("carteira"))
         print("Sintomas:", pacientesNaoUrgentes[0].get("sintomas"))
 
         pacienteAtendidos.append(pacientesNaoUrgentes[0])
         pacientesNaoUrgentes.pop(0)
+        internalPaciente = 0
         print("Paciente Atendido com sucesso!\n")
-    print("Triagem finalizada com sucesso!\n")
-    menu()
+        return menu()
+        
+    else:
+        print("Erro no software\n")
+        return menu()
     
-
 
 print("Bem vindo ao ao sistema clinico de triagem:")
 menu()
